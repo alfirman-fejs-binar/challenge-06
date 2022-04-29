8eadd44e513147296a30bcfdbcd05840c0ea2794
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-04-29 14:47:22 +0700

    routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
 import { Route, Routes as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export default function Routes() {
   return (
     <Router>
-      <Route path="/" element={<p>home</p>} />
-      <Route path="/register" element={<p>register</p>} />
-      <Route path="/login" element={<p>login</p>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
     </Router>
   );
 }
