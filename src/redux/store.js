fa35f0bb8f264f94e3074a762df17dbee51bc21c
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-05-13 20:06:23 +0700

    remove unsused code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slice/counterSlice";
 import carReducer from "./slice/carSlice";
 import authReducer from "./slice/authSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     car: carReducer,
     auth: authReducer,
   },
